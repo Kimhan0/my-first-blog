69406c1dfde17423365ea6d502719c2c3f1d6694
cea1063 <cea1063@gmial.com>
2018-05-18 15:56:10 +0900

    Third commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '#+=#i5j_-v6mn1im$0@21o=k)!*qw$dml0uizu=d)nqry1!4tq'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]', '.pythonanywhere.com']
 
 
 # Application definition
